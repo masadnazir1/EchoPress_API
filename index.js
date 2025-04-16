b8952dec068393602bd7985a7acc1462bdbc754e
masadnazir1 <masadnazir1@gmail.com>
2025-04-16 13:25:02 +0500

    added the cors

--- index.js
+++ index.js
@@ -13,7 +13,13 @@ const app = express();
 const port = process.env.PORT || 5000;
 
 // Middleware
-app.use(cors());
+app.use(
+  cors({
+    origin: process.env.CORS_ORIGIN || "*" || "https://blogbook.galaxydev.pk/*",
+    methods: ["GET", "POST", "PUT", "DELETE"],
+    allowedHeaders: ["Content-Type", "Authorization"],
+  })
+);
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 //
